663ca59fec4467c1519664219595c37148462e6e
David Santiago Avellaneda Montero <avellaneda.dev@gmail.com>
2023-08-23 19:37:00 -0500

    Setting up initial settings

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["rickandmortyapi.com"],
+    deviceSizes: [400, 600],
+  },
+};
+
+module.exports = nextConfig;
